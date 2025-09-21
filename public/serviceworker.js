487f72ee2e0ab72a6f88d03651f2c11e755bb48c
VeluxIa Tecnologia (via WeWeb) <veluxiatecnologia@gmail.com>
2025-09-20 21:41:24 -0300

    v69 - Veluxia

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,4 +1,4 @@
-const version = 51;
+const version = 69;
 self.addEventListener('install', event => {
     // eslint-disable-next-line no-console
     console.log(`Service worker v${version} installed`);
